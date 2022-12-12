8ae60ae89fda5acdb7e7ccf013483ab081ed046b
AkshaySyntellect <akshaymarale3@gmail.com>
2022-12-12 19:11:53 +0530

    first project

---
+++ Developers/AkshayMarale/akshay/akshay/src/test/java/com/training/akshay/AkshayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.akshay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkshayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
