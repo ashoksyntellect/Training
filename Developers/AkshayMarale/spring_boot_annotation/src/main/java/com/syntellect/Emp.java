edeef92f4fd8fc133169d8f2d21c9e9524e3dbb8
AkshaySyntellect <akshaymarale3@gmail.com>
2022-12-13 18:24:45 +0530

    annotation base

---
+++ Developers/AkshayMarale/spring_boot_annotation/src/main/java/com/syntellect/Emp.java
@@ -0,0 +1,12 @@
+package com.syntellect;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Emp {
+
+	public void whatsYourName()
+	{
+		System.out.println("My Name Is AKki");
+	}
+}
