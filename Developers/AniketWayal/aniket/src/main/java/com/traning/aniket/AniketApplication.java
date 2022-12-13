281b442929e10ced415ba14d10945a8b6dbdfa08
NWNB2378 <aniketwayal26@gmail.com>
2022-12-13 12:48:22 +0530

    first project

---
+++ Developers/AniketWayal/aniket/src/main/java/com/traning/aniket/AniketApplication.java
@@ -0,0 +1,13 @@
+package com.traning.aniket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniketApplication.class, args);
+	}
+
+}
---
+++ Developers/AniketWayal/aniket/src/test/java/com/traning/aniket/AniketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traning.aniket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
