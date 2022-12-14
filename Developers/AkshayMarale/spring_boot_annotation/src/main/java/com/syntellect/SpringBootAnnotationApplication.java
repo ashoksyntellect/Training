2e49dcadc469e76c18e8123dfced35952048c429
AkshaySyntellect <akshaymarale3@gmail.com>
2022-12-15 00:06:20 +0530

    OneTomany Project

--- Developers/AkshayMarale/spring_boot_annotation/src/main/java/com/syntellect/SpringBootAnnotationApplication.java
+++ Developers/AkshayMarale/spring_boot_annotation/src/main/java/com/syntellect/SpringBootAnnotationApplication.java
@@ -1,12 +1,24 @@
 package com.syntellect;
 
+
 import java.util.Date;
+
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.hibrelation.Laptop;
+import com.hibrelation.Student1;
 
 import mypack.Dog;
 
@@ -25,9 +37,35 @@ public class SpringBootAnnotationApplication implements CommandLineRunner {
 	@Autowired
 	private Dog dog;
 	
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(SpringBootAnnotationApplication.class, args);
+		
+		Laptop laptop =new Laptop();
+		laptop.setId(101);
+		laptop.setLname("Dell");
+		
+		Student1 s = new Student1();
+		s.setRollno(1);
+		s.setName("a");
+		s.setMarks(90);
+		s.getLaptop().add(laptop);
+		
+		Configuration con = new Configuration().configure().addAnnotatedClass(Student1.class).addAnnotatedClass(Laptop.class);
+		
+		ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
+		
+		SessionFactory sf=con.buildSessionFactory(registry);
+		
+		Session session = sf.openSession();
+		
+		session.beginTransaction();
+		
+		session.save(laptop);
+		session.save(s);
+		
+		session.getTransaction().commit();
 	}
 
 	@Override
